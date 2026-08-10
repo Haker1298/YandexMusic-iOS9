9983757a529ac5301593cf5026b24cf1db6aeec8
Z User <z@container>
2026-08-10 07:51:49 +0000

    fix icons: wave 2x, correct explore, new concert, heart collection

--- AppDelegate.mm
+++ AppDelegate.mm
@@ -19,55 +19,34 @@ static UIImage *halfSizeImage(NSString *name) {
     return result;
 }
 
-static UIImage *concertIcon(CGFloat size, BOOL active) {
-    UIGraphicsBeginImageContextWithOptions(CGSizeMake(size, size), NO, 0);
-    UIColor *color = active ? [UIColor colorWithRed:1.0 green:0.8 blue:0.0 alpha:1.0] : [UIColor colorWithWhite:0.6 alpha:1.0];
-    [color setStroke];
-    [color setFill];
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    // Microphone body (rounded rect via UIBezierPath)
-    CGFloat cx = size / 2, cy = size * 0.38;
-    CGFloat rw = size * 0.16, rh = size * 0.28;
-    UIBezierPath *micBody = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(cx - rw, cy - rh, rw * 2, rh * 2) cornerRadius:rw];
-    [micBody fill];
-    // Mic stand arc
-    CGFloat standY = cy + rh;
-    CGContextSetLineWidth(ctx, 1.5);
-    CGContextBeginPath(ctx);
-    CGContextMoveToPoint(ctx, cx - size * 0.25, standY + size * 0.12);
-    CGContextAddQuadCurveToPoint(ctx, cx - size * 0.25, standY + size * 0.25, cx, standY + size * 0.25);
-    CGContextAddQuadCurveToPoint(ctx, cx + size * 0.25, standY + size * 0.25, cx + size * 0.25, standY + size * 0.12);
-    CGContextStrokePath(ctx);
-    // Stand line
-    CGContextBeginPath(ctx);
-    CGContextMoveToPoint(ctx, cx, standY + size * 0.25);
-    CGContextAddLineToPoint(ctx, cx, size * 0.88);
-    CGContextStrokePath(ctx);
-    // Base
-    CGContextBeginPath(ctx);
-    CGContextMoveToPoint(ctx, cx - size * 0.2, size * 0.88);
-    CGContextAddLineToPoint(ctx, cx + size * 0.2, size * 0.88);
-    CGContextStrokePath(ctx);
-    UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return result;
-}
-
-static UIImage *collectionIcon(CGFloat size, BOOL active) {
+static UIImage *heartIcon(CGFloat size, BOOL active) {
     UIGraphicsBeginImageContextWithOptions(CGSizeMake(size, size), NO, 0);
     UIColor *color = active ? [UIColor colorWithRed:1.0 green:0.8 blue:0.0 alpha:1.0] : [UIColor colorWithWhite:0.6 alpha:1.0];
     [color setFill];
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGFloat m = size * 0.18;
-    CGFloat cw = (size - m * 3) / 2;
-    CGFloat ch = cw * 1.3;
-    CGFloat baseY = size * 0.15;
-    // Stack of 3 items
-    for (int i = 0; i < 3; i++) {
-        CGFloat x = m + i * (cw * 0.15);
-        CGFloat y = baseY + i * (ch * 0.2);
-        CGContextFillRect(ctx, CGRectMake(x, y, cw, ch));
-    }
+    // Heart shape using UIBezierPath
+    CGFloat s = size;
+    UIBezierPath *heart = [UIBezierPath bezierPath];
+    CGFloat cx = s / 2;
+    // Start at bottom point
+    [heart moveToPoint:CGPointMake(cx, s * 0.85)];
+    // Left curve
+    [heart addCurveToPoint:CGPointMake(cx - s * 0.55, s * 0.15)
+          controlPoint1:CGPointMake(cx - s * 0.55, s * 0.55)
+          controlPoint2:CGPointMake(cx - s * 0.55, s * 0.3)];
+    // Left top bump
+    [heart addCurveToPoint:CGPointMake(cx, s * 0.25)
+          controlPoint1:CGPointMake(cx - s * 0.25, s * -0.05)
+          controlPoint2:CGPointMake(cx - s * 0.05, s * 0.15)];
+    // Right top bump
+    [heart addCurveToPoint:CGPointMake(cx + s * 0.55, s * 0.15)
+          controlPoint1:CGPointMake(cx + s * 0.05, s * 0.15)
+          controlPoint2:CGPointMake(cx + s * 0.25, s * -0.05)];
+    // Right curve
+    [heart addCurveToPoint:CGPointMake(cx, s * 0.85)
+          controlPoint1:CGPointMake(cx + s * 0.55, s * 0.3)
+          controlPoint2:CGPointMake(cx + s * 0.55, s * 0.55)];
+    [heart closePath];
+    [heart fill];
     UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return result;
@@ -109,11 +88,11 @@ static UIImage *collectionIcon(CGFloat size, BOOL active) {
     self.tabBarController = [[UITabBarController alloc] init];
     self.tabBarController.delegate = (id)self;
 
-    // Tab 1: Моя Волна
+    // Tab 1: Моя Волна (icon 2x bigger, no halfSize)
     WebViewController *waveVC = [[WebViewController alloc] initWithPage:@"wave" title:@"Моя Волна"];
     UINavigationController *waveNav = [[UINavigationController alloc] initWithRootViewController:waveVC];
-    waveNav.tabBarItem.image = halfSizeImage(@"tab_wave");
-    waveNav.tabBarItem.selectedImage = halfSizeImage(@"tab_wave_active");
+    waveNav.tabBarItem.image = [UIImage imageNamed:@"tab_wave"];
+    waveNav.tabBarItem.selectedImage = [UIImage imageNamed:@"tab_wave_active"];
     waveNav.tabBarItem.title = @"Моя волна";
     waveNav.navigationBarHidden = YES;
 
@@ -128,26 +107,16 @@ static UIImage *collectionIcon(CGFloat size, BOOL active) {
     // Tab 3: Концерты
     WebViewController *concertsVC = [[WebViewController alloc] initWithPage:@"concerts" title:@"Концерты"];
     UINavigationController *concertsNav = [[UINavigationController alloc] initWithRootViewController:concertsVC];
-    UIImage *concertImg = halfSizeImage(@"tab_concert");
-    UIImage *concertImgActive = halfSizeImage(@"tab_concert_active");
-    if (concertImg) {
-        concertsNav.tabBarItem.image = concertImg;
-    } else {
-        concertsNav.tabBarItem.image = concertIcon(25, NO);
-    }
-    if (concertImgActive) {
-        concertsNav.tabBarItem.selectedImage = concertImgActive;
-    } else {
-        concertsNav.tabBarItem.selectedImage = concertIcon(25, YES);
-    }
+    concertsNav.tabBarItem.image = halfSizeImage(@"tab_concert");
+    concertsNav.tabBarItem.selectedImage = halfSizeImage(@"tab_concert_active");
     concertsNav.tabBarItem.title = @"Концерты";
     concertsNav.navigationBarHidden = YES;
 
-    // Tab 4: Коллекция
+    // Tab 4: Коллекция (heart icon)
     WebViewController *collectionVC = [[WebViewController alloc] initWithPage:@"collection" title:@"Коллекция"];
     UINavigationController *collectionNav = [[UINavigationController alloc] initWithRootViewController:collectionVC];
-    collectionNav.tabBarItem.image = collectionIcon(25, NO);
-    collectionNav.tabBarItem.selectedImage = collectionIcon(25, YES);
+    collectionNav.tabBarItem.image = heartIcon(25, NO);
+    collectionNav.tabBarItem.selectedImage = heartIcon(25, YES);
     collectionNav.tabBarItem.title = @"Коллекция";
     collectionNav.navigationBarHidden = YES;
 
