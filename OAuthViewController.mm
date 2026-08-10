f06ca4405e0a6654134f60cc8c65394dd4e97b06
Z User <z@container>
2026-08-10 05:54:38 +0000

    improve OAuth: Yandex ID login is now primary, manual token is hidden option
    - Login button directly opens Yandex ID WebView
    - Manual token input moved to small link at bottom
    - Uses custom URL scheme (yandexmusic://) for token redirect
    - Better error handling when OAuth fails
    - Updated placeholder to show y0_ token format

--- OAuthViewController.mm
+++ OAuthViewController.mm
@@ -3,6 +3,7 @@
 #import "KeychainHelper.h"
 
 static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
+static NSString *const kRedirectURI = @"yandexmusic://auth/callback";
 
 @implementation OAuthViewController {
     UIView *loginView;
@@ -31,7 +32,7 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     UIImageView *logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"app_icon"]];
     if (logo.image) {
         CGFloat s = 90;
-        logo.frame = CGRectMake((cx - s) / 2, cy * 0.18, s, s);
+        logo.frame = CGRectMake((cx - s) / 2, cy * 0.15, s, s);
         logo.contentMode = UIViewContentModeScaleAspectFit;
         logo.layer.cornerRadius = 20;
         logo.layer.masksToBounds = YES;
@@ -39,41 +40,57 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     }
     
     // Title
-    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, cy * 0.18 + 110, cx, 30)];
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, cy * 0.15 + 108, cx, 30)];
     titleLabel.text = @"\u042F\u043D\u0434\u0435\u043A\u0441 \u041C\u0443\u0437\u044B\u043A\u0430";
     titleLabel.textAlignment = NSTextAlignmentCenter;
     titleLabel.textColor = [UIColor whiteColor];
     titleLabel.font = [UIFont boldSystemFontOfSize:22];
     [loginView addSubview:titleLabel];
     
-    // Buttons at the bottom
-    CGFloat bottomY = cy - 140;
+    // Subtitle
+    UILabel *subLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, cy * 0.15 + 140, cx, 20)];
+    subLabel.text = @"\u0412\u0445\u043E\u0434 \u0447\u0435\u0440\u0435\u0437 \u042F\u043D\u0434\u0435\u043A\u0441 ID";
+    subLabel.textAlignment = NSTextAlignmentCenter;
+    subLabel.textColor = [UIColor colorWithWhite:0.5 alpha:1.0];
+    subLabel.font = [UIFont systemFontOfSize:14];
+    [loginView addSubview:subLabel];
     
-    // Button 1: Vhod (login)
+    CGFloat bottomY = cy - 160;
+    
+    // Button 1: Vhod cherez Yandex ID (primary)
     UIButton *loginBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    loginBtn.frame = CGRectMake(30, bottomY, cx - 60, 48);
+    loginBtn.frame = CGRectMake(30, bottomY, cx - 60, 50);
     loginBtn.backgroundColor = [UIColor colorWithRed:1.0 green:0.8 blue:0.0 alpha:1.0];
-    [loginBtn setTitle:@"\u0412\u043E\u0439\u0442\u0438" forState:UIControlStateNormal];
+    [loginBtn setTitle:@"\u0412\u043E\u0439\u0442\u0438 \u0447\u0435\u0440\u0435\u0437 \u042F\u043D\u0434\u0435\u043A\u0441 ID" forState:UIControlStateNormal];
     [loginBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     loginBtn.titleLabel.font = [UIFont boldSystemFontOfSize:17];
-    loginBtn.layer.cornerRadius = 10;
+    loginBtn.layer.cornerRadius = 12;
     loginBtn.clipsToBounds = YES;
-    [loginBtn addTarget:self action:@selector(showTokenInput) forControlEvents:UIControlEventTouchUpInside];
+    [loginBtn addTarget:self action:@selector(showWebViewLogin) forControlEvents:UIControlEventTouchUpInside];
     [loginView addSubview:loginBtn];
     
     // Button 2: Guest mode
     UIButton *guestBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    guestBtn.frame = CGRectMake(30, bottomY + 62, cx - 60, 48);
-    guestBtn.backgroundColor = [UIColor colorWithWhite:0.18 alpha:1.0];
+    guestBtn.frame = CGRectMake(30, bottomY + 66, cx - 60, 44);
+    guestBtn.backgroundColor = [UIColor colorWithWhite:0.15 alpha:1.0];
     [guestBtn setTitle:@"\u0413\u043E\u0441\u0442\u0435\u0432\u043E\u0439 \u0440\u0435\u0436\u0438\u043C" forState:UIControlStateNormal];
-    [guestBtn setTitleColor:[UIColor colorWithWhite:0.7 alpha:1.0] forState:UIControlStateNormal];
-    guestBtn.titleLabel.font = [UIFont systemFontOfSize:16];
+    [guestBtn setTitleColor:[UIColor colorWithWhite:0.6 alpha:1.0] forState:UIControlStateNormal];
+    guestBtn.titleLabel.font = [UIFont systemFontOfSize:15];
     guestBtn.layer.cornerRadius = 10;
-    guestBtn.layer.borderColor = [UIColor colorWithWhite:0.3 alpha:1.0].CGColor;
+    guestBtn.layer.borderColor = [UIColor colorWithWhite:0.25 alpha:1.0].CGColor;
     guestBtn.layer.borderWidth = 1;
     guestBtn.clipsToBounds = YES;
     [guestBtn addTarget:self action:@selector(enterGuestMode) forControlEvents:UIControlEventTouchUpInside];
     [loginView addSubview:guestBtn];
+    
+    // Manual token link
+    UIButton *manualBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    manualBtn.frame = CGRectMake(0, bottomY + 124, cx, 30);
+    [manualBtn setTitle:@"\u0412\u0432\u0435\u0441\u0442\u0438 \u0442\u043E\u043A\u0435\u043D \u0432\u0440\u0443\u0447\u043D\u0443\u044E" forState:UIControlStateNormal];
+    [manualBtn setTitleColor:[UIColor colorWithWhite:0.35 alpha:1.0] forState:UIControlStateNormal];
+    manualBtn.titleLabel.font = [UIFont systemFontOfSize:12];
+    [manualBtn addTarget:self action:@selector(showTokenInput) forControlEvents:UIControlEventTouchUpInside];
+    [loginView addSubview:manualBtn];
     
     [self.view addSubview:loginView];
 }
@@ -89,7 +106,6 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     
     CGFloat cx = self.view.bounds.size.width;
     
-    // Back button
     UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeSystem];
     backBtn.frame = CGRectMake(10, 20, 70, 36);
     [backBtn setTitle:@"< \u041D\u0430\u0437\u0430\u0434" forState:UIControlStateNormal];
@@ -98,7 +114,6 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     [backBtn addTarget:self action:@selector(backToLoginFromToken) forControlEvents:UIControlEventTouchUpInside];
     [tokenInputView addSubview:backBtn];
     
-    // Title
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, cx, 28)];
     titleLabel.text = @"\u0412\u0432\u043E\u0434 \u0442\u043E\u043A\u0435\u043D\u0430";
     titleLabel.textAlignment = NSTextAlignmentCenter;
@@ -106,22 +121,20 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     titleLabel.font = [UIFont boldSystemFontOfSize:20];
     [tokenInputView addSubview:titleLabel];
     
-    // Token text field
     UITextField *tokenField = [[UITextField alloc] initWithFrame:CGRectMake(30, 130, cx - 60, 40)];
-    tokenField.placeholder = @"AQAAAAA...";
+    tokenField.placeholder = @"y0_AgAAAAA...";
     tokenField.textColor = [UIColor whiteColor];
     tokenField.backgroundColor = [UIColor colorWithWhite:0.13 alpha:1.0];
     tokenField.layer.cornerRadius = 8;
     tokenField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 12, 40)];
     tokenField.leftViewMode = UITextFieldViewModeAlways;
-    tokenField.font = [UIFont systemFontOfSize:14];
+    tokenField.font = [UIFont systemFontOfSize:13];
     tokenField.returnKeyType = UIReturnKeyDone;
     tokenField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     tokenField.autocorrectionType = UITextAutocorrectionTypeNo;
     tokenField.tag = 999;
     [tokenInputView addSubview:tokenField];
     
-    // Submit button
     UIButton *submitBtn = [UIButton buttonWithType:UIButtonTypeSystem];
     submitBtn.frame = CGRectMake(30, 185, cx - 60, 44);
     submitBtn.backgroundColor = [UIColor colorWithRed:1.0 green:0.8 blue:0.0 alpha:1.0];
@@ -133,22 +146,8 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     [submitBtn addTarget:self action:@selector(submitManualToken) forControlEvents:UIControlEventTouchUpInside];
     [tokenInputView addSubview:submitBtn];
     
-    // Get token button - opens WebView
-    UIButton *linkBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    linkBtn.frame = CGRectMake(30, 245, cx - 60, 36);
-    linkBtn.backgroundColor = [UIColor colorWithWhite:0.15 alpha:1.0];
-    linkBtn.layer.cornerRadius = 8;
-    linkBtn.layer.borderColor = [UIColor colorWithWhite:0.25 alpha:1.0].CGColor;
-    linkBtn.layer.borderWidth = 1;
-    [linkBtn setTitle:@"\u041F\u043E\u043B\u0443\u0447\u0438\u0442\u044C \u0442\u043E\u043A\u0435\u043D" forState:UIControlStateNormal];
-    [linkBtn setTitleColor:[UIColor colorWithRed:1.0 green:0.8 blue:0.0 alpha:1.0] forState:UIControlStateNormal];
-    linkBtn.titleLabel.font = [UIFont systemFontOfSize:13];
-    [linkBtn addTarget:self action:@selector(showWebViewLogin) forControlEvents:UIControlEventTouchUpInside];
-    [tokenInputView addSubview:linkBtn];
-    
-    // Help text
-    UILabel *helpLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 300, cx - 60, 80)];
-    helpLabel.text = @"\u041D\u0430\u0436\u043C\u0438\u0442\u0435 \u043A\u043D\u043E\u043F\u043A\u0443 \u0432\u044B\u0448\u0435, \u0432\u043E\u0439\u0434\u0438\u0442\u0435 \u0432 \u042F\u043D\u0434\u0435\u043A\u0441, \u0442\u043E\u043A\u0435\u043D \u0431\u0443\u0434\u0435\u0442 \u0441\u043A\u043E\u043F\u0438\u0440\u043E\u0432\u0430\u043D \u0430\u0432\u0442\u043E\u043C\u0430\u0442\u0438\u0447\u0435\u0441\u043A\u0438";
+    UILabel *helpLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 250, cx - 60, 60)];
+    helpLabel.text = @"\u0412\u0441\u0442\u0430\u0432\u044C\u0442\u0435 OAuth \u0442\u043E\u043A\u0435\u043D \u043E\u0442 \u042F\u043D\u0434\u0435\u043A\u0441 ID";
     helpLabel.numberOfLines = 0;
     helpLabel.textAlignment = NSTextAlignmentCenter;
     helpLabel.textColor = [UIColor colorWithWhite:0.4 alpha:1.0];
@@ -183,9 +182,10 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     [del showMainApp];
 }
 
-#pragma mark - WebView Login
+#pragma mark - WebView Login (Yandex ID)
 
 - (void)showWebViewLogin {
+    [loginView removeFromSuperview];
     [tokenInputView removeFromSuperview];
     
     webViewContainer = [[UIView alloc] initWithFrame:self.view.bounds];
@@ -200,13 +200,21 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     [backBtn addTarget:self action:@selector(backToLogin) forControlEvents:UIControlEventTouchUpInside];
     [webViewContainer addSubview:backBtn];
     
+    UILabel *loadingLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 55, self.view.bounds.size.width, 20)];
+    loadingLabel.text = @"\u0417\u0430\u0433\u0440\u0443\u0437\u043A\u0430 \u042F\u043D\u0434\u0435\u043A\u0441 ID...";
+    loadingLabel.textAlignment = NSTextAlignmentCenter;
+    loadingLabel.textColor = [UIColor colorWithWhite:0.5 alpha:1.0];
+    loadingLabel.font = [UIFont systemFontOfSize:13];
+    loadingLabel.tag = 43;
+    [webViewContainer addSubview:loadingLabel];
+    
     UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
-    spinner.center = CGPointMake(self.view.bounds.size.width / 2, 70);
+    spinner.center = CGPointMake(self.view.bounds.size.width / 2, 35);
     spinner.tag = 42;
     [webViewContainer addSubview:spinner];
     [spinner startAnimating];
     
-    CGFloat webY = 56;
+    CGFloat webY = 20;
     authWebView = [[WKWebView alloc] initWithFrame:CGRectMake(0, webY, self.view.bounds.size.width, self.view.bounds.size.height - webY)];
     authWebView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     authWebView.navigationDelegate = self;
@@ -216,8 +224,8 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     [self.view addSubview:webViewContainer];
     
     NSString *authURL = [NSString stringWithFormat:
-        @"https://oauth.yandex.ru/authorize?response_type=token&client_id=%@&redirect_uri=https://oauth.yandex.ru/verification_code&display=mobile",
-        kClientId
+        @"https://oauth.yandex.ru/authorize?response_type=token&client_id=%@&redirect_uri=%@&display=mobile",
+        kClientId, kRedirectURI
     ];
     NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:authURL]];
     [authWebView loadRequest:req];
@@ -227,8 +235,25 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     NSURL *url = navigationAction.request.URL;
     NSString *absString = [url absoluteString];
     
+    // Intercept our custom URL scheme with token
+    if ([[url scheme] isEqualToString:@"yandexmusic"]) {
+        NSString *fragment = [url fragment];
+        if (fragment) {
+            NSString *token = [self extractToken:fragment];
+            if (token) {
+                [self tokenReceived:token];
+                decisionHandler(WKNavigationActionPolicyCancel);
+                return;
+            }
+        }
+        decisionHandler(WKNavigationActionPolicyCancel);
+        return;
+    }
+    
+    // Also check for token in any URL (fallback)
     if ([absString containsString:@"access_token="]) {
         NSString *fragment = [url fragment];
+        if (!fragment) fragment = [url query];
         if (fragment) {
             NSString *token = [self extractToken:fragment];
             if (token) {
@@ -239,7 +264,7 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
         }
     }
     
-    // If redirected to verification_code page, try to extract token from URL
+    // If redirected to verification_code page, extract token from URL
     if ([absString containsString:@"oauth.yandex.ru/verification_code"]) {
         NSString *fragment = [url fragment];
         if (fragment && [fragment containsString:@"access_token="]) {
@@ -250,18 +275,16 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
                 return;
             }
         }
-        // If we got to verification_code without token in fragment, show message
-        if (!fragment || ![fragment containsString:@"access_token="]) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"\u0422\u043E\u043A\u0435\u043D"
-                                                               message:@"\u0421\u043A\u043E\u043F\u0438\u0440\u0443\u0439\u0442\u0435 access_token \u0438\u0437 \u0430\u0434\u0440\u0435\u0441\u0430 \u0441\u0442\u0440\u0430\u043D\u0438\u0446\u044B \u0438 \u0432\u0441\u0442\u0430\u0432\u044C\u0442\u0435 \u043D\u0430 \u043F\u0440\u0435\u0434\u044B\u0434\u0443\u0449\u0435\u043C \u044D\u043A\u0440\u0430\u043D\u0435"
-                                                              delegate:nil
-                                                     cancelButtonTitle:@"OK"
-                                                     otherButtonTitles:nil];
-                [alert show];
-                [self backToLogin];
-            });
-        }
+        // No token in fragment - Yandex showed verification page instead
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"\u0412\u043D\u0438\u043C\u0430\u043D\u0438\u0435"
+                                                           message:@"\u0410\u0432\u0442\u043E\u0440\u0438\u0437\u0430\u0446\u0438\u044F \u043D\u0435 \u0437\u0430\u0432\u0435\u0440\u0448\u0435\u043D\u0430. \u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0442\u043E\u043A\u0435\u043D \u0432\u0440\u0443\u0447\u043D\u0443\u044E \u0438\u043B\u0438 \u043F\u043E\u043F\u0440\u043E\u0431\u0443\u0439\u0442\u0435 \u0435\u0449\u0451 \u0440\u0430\u0437."
+                                                          delegate:nil
+                                                 cancelButtonTitle:@"OK"
+                                                 otherButtonTitles:nil];
+            [alert show];
+            [self backToLogin];
+        });
         decisionHandler(WKNavigationActionPolicyCancel);
         return;
     }
@@ -272,11 +295,14 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
     UIActivityIndicatorView *spinner = (UIActivityIndicatorView *)[webViewContainer viewWithTag:42];
     if (spinner) [spinner stopAnimating];
+    UILabel *loadingLabel = (UILabel *)[webViewContainer viewWithTag:43];
+    if (loadingLabel) loadingLabel.hidden = YES;
     
     // Try to extract token from current URL
     NSString *url = [webView.URL absoluteString];
     if ([url containsString:@"access_token="]) {
         NSString *fragment = [webView.URL fragment];
+        if (!fragment) fragment = [webView.URL query];
         if (fragment) {
             NSString *token = [self extractToken:fragment];
             if (token) {
@@ -303,13 +329,13 @@ static NSString *const kClientId = @"b399db89f01e4bd4965cef1f7973ee05";
     [webViewContainer removeFromSuperview];
     webViewContainer = nil;
     authWebView = nil;
-    [self showTokenInput];
+    [self buildLoginView];
 }
 
 - (void)backToLoginFromToken {
     [tokenInputView removeFromSuperview];
     tokenInputView = nil;
-    [self.view addSubview:loginView];
+    [self buildLoginView];
 }
 
 - (void)tokenReceived:(NSString *)token {
